b33409597eda9943e302125c7ee23c2c7ed1c4dd
K Mahalakshmi <109159428+kmahalakshmi2004@users.noreply.github.com>
2024-05-17 00:00:20 +0530

    Time: 56 ms (31.44%) | Memory: 49 MB (13.92%) - LeetSync

--- 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -6,7 +6,7 @@ const expect = (val) => {
     const throwError = (errorStr) => {throw new Error(errorStr)};
     
     return {
-        toBe:    (val2) => val2 === val || throwError('Not Equal'),
+        toBe:(val2) => val2 === val || throwError('Not Equal'),
         notToBe: (val2) => val2 !== val || throwError('Equal'),
     };
 };
