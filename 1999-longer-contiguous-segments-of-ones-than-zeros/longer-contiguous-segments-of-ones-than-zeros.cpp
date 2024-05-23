9fa7c1a9c82d44d55fbf4f53f2f565ed23f24ce2
K Mahalakshmi <109159428+kmahalakshmi2004@users.noreply.github.com>
2024-05-24 00:34:22 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (83.16%) - LeetSync

--- 1999-longer-contiguous-segments-of-ones-than-zeros/longer-contiguous-segments-of-ones-than-zeros.cpp
+++ 1999-longer-contiguous-segments-of-ones-than-zeros/longer-contiguous-segments-of-ones-than-zeros.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
     bool checkZeroOnes(string s) {
-        int count1=0;
-        int count2=0;
-       int max1=0;int max2=0;
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]=='1')
-            {
-                count1++;
-                if(s[i+1]!=s[i])
-                {
-                    max1=max(count1,max1);
-                    count1=0;
-                }
+        int cnt0=0;
+        int cnt1=0; 
+        int max0=0; 
+        int max1=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='1'){
+                cnt1++;
+                cnt0=0;
+                max1=max(cnt1,max1);
             }
-            
-            if(s[i]=='0')
-            {
-                count2++;
-                if(s[i+1]!=s[i])
-                {
-                    max2=max(count2,max2);
-                    count2=0;
-                }
+            else{
+                cnt0++;
+                cnt1=0;
+                max0=max(cnt0,max0);
             }
         }
-        
-        if(max1>max2)
-            return true;
-        return false;
-        
-        
+        return max1>max0;
     }
 };
