9d7490bc73d344bf0c90f6674533411048e48995
K Mahalakshmi <109159428+kmahalakshmi2004@users.noreply.github.com>
2024-05-25 00:00:23 +0530

    Time: 45 ms (87.67%) | Memory: 48.9 MB (30.09%) - LeetSync

--- 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
@@ -4,9 +4,8 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    const returnedArray=[];
     for(let i=0;i<arr.length;i++){
-        returnedArray[i]=fn(arr[i],i);
+        arr[i]=fn(arr[i],i);
     }
-    return returnedArray;
+    return arr;
 };
