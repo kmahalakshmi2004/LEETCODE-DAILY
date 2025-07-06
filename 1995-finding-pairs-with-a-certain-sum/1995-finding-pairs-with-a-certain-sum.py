be3c3baac9a8865fcb1158715c8a07e73aaaaacf
K Mahalakshmi <109159428+kmahalakshmi2004@users.noreply.github.com>
2025-07-06 23:59:47 +0530

    Time: 396 ms (18.98%), Space: 48.2 MB (82.48%) - LeetHub

---
+++ 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.py
@@ -0,0 +1,16 @@
+class FindSumPairs:
+    def __init__(self, nums1: List[int], nums2: List[int]):
+        self.nums1 = nums1
+        self.nums2 = nums2
+        self.freq = Counter(nums2)
+
+    def add(self, index: int, val: int) -> None:
+        self.freq[self.nums2[index]] -= 1  # Remove old one
+        self.nums2[index] += val
+        self.freq[self.nums2[index]] += 1  # Count new one
+
+    def count(self, tot: int) -> int:
+        ans = 0
+        for a in self.nums1:  
+            ans += self.freq[tot - a]  # a + b = tot -> b = tot - a
+        return ans
